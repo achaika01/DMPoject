e49f34566f98b44bab4778368f39bf9f02e16e48
achaika01 <achaika@kse.org.ua>
2025-04-11 21:25:46 +0300

    graph can be generated automatically now

--- DMProject/Graph.cs
+++ DMProject/Graph.cs
@@ -82,4 +82,54 @@ public class Graph
             Console.WriteLine();
         }
     }
+
+    public void Clear()
+    {
+        vertices.Clear();
+        adjacencyMatrix = new int[0, 0];
+    }
+
+    public void GenerateRandomGraph(int vertexCount, double density)
+    {
+        if (density < 0 || density > 1)
+        {
+            Console.WriteLine("Щільність має бути в межах від 0 до 1.");
+            return;
+        }
+
+        for (int i = 0; i < vertexCount; i++)
+        {
+            AddVertex("V" + i);
+        }
+
+        int maxEdges = vertexCount * (vertexCount - 1) / 2;
+        int targetEdgeCount = (int)(density * maxEdges);
+        int edgeCount = 0;
+
+        Random rand = new Random();
+        HashSet<(int, int)> addedEdges = new HashSet<(int, int)>();
+
+        while (edgeCount < targetEdgeCount)
+        {
+            int i = rand.Next(vertexCount);
+            int j = rand.Next(vertexCount);
+
+            if (i == j) continue;
+
+            int a = Math.Min(i, j);
+            int b = Math.Max(i, j);
+
+            if (addedEdges.Contains((a, b))) continue;
+
+            adjacencyMatrix[a, b] = 1;
+            adjacencyMatrix[b, a] = 1;
+
+            addedEdges.Add((a, b));
+            edgeCount++;
+        }
+
+        Console.WriteLine($"Згенеровано граф з {vertexCount} вершинами і {edgeCount} ребрами (щільність ≈ {density:P0})");
+    }
+
+
 }
